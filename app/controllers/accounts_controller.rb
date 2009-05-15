ec63461e3c4aa1088a9b7eb043de0734f024441b
Luiz Octavio de Almeida Soares <luiz@virtual.(none)>
2009-05-15 17:27:55 -0300

    Placeholder for view accounts#index

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,12 @@
 class AccountsController < ApplicationController
+    
+    def index
+        @accounts = Account.find(:all)
+    end
+
+    def new
+        @account = Account.new
+    end
+
+    
 end
